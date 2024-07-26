429dd23128ccc63bc08891d549bae0768a9b9ac1
Carina <carina.bolus@gws.ms>
2024-07-26 10:44:22 +0000

    and more

--- src/shopware_api_client/endpoints/store/core/cart.py
+++ src/shopware_api_client/endpoints/store/core/cart.py
@@ -1,7 +1,9 @@
 from typing import Any
 
 from shopware_api_client.base import ApiModelBase, EndpointBase, EndpointClass
-from shopware_api_client.endpoints.base_fields import IdField, Price
+from shopware_api_client.endpoints.admin.core.country import Country
+from shopware_api_client.endpoints.base_fields import BaseFieldSet, IdField, Price
+from shopware_api_client.endpoints.store.core.address import Address
 
 
 class ItemPrice(ApiModelBase[EndpointClass]):
@@ -13,12 +15,39 @@ class ItemPrice(ApiModelBase[EndpointClass]):
     reference_price: float | None = None
     list_price: float | None = None
     regulation_price: float | None = None
+    
+
+class ShippingCosts(ItemPrice):
+    raw_total: float | None = None
+    position_price: float | None = None
+    net_price: float | None = None
+    has_range: bool | None = None
+    variant: str | None = None
+
+
+class ItemPayload(BaseFieldSet):
+    is_closeout: bool
+    release_date: str | None = None
+    is_new: bool
+    mark_as_topseller: bool
+    product_number: str
+    manufacturer_id: IdField | None = None
+    tax_id: IdField
+    category_ids: list[IdField]
+    property_ids: list[IdField] | None = None
+    option_ids: list[IdField] | None = None
+    options: list[dict[str, Any]] | None = None
+    stream_ids: list[IdField] | None = None
+    parent_id: IdField | None = None
+    stock: int
+    features: list[dict[str, Any]] | None = None
+    gws_comment: str | None = None
 
 
 class LineItem(ApiModelBase[EndpointClass]):
     _identifier: str = "cart_line_item"
     
-    payload: dict[str, Any]
+    payload: ItemPayload #dict[str, Any]
     label: str
     quantity: int
     price_definition: dict[str, Any]
@@ -39,22 +68,24 @@ class LineItem(ApiModelBase[EndpointClass]):
     referenced_id: IdField
 
 
-class Position(ApiModelBase[EndpointClass]):
-    _identifier: str = "cart_delivery_position"
-    
-    delivery_date: str
-    line_items: list[LineItem]
+class DeliveryPosition(BaseFieldSet):
+    delivery_date: Any
+    line_item: LineItem
     price: ItemPrice
 
 
-class CartDelivery(ApiModelBase[EndpointClass]):
-    _identifier: str = "cart_delivery"
+class Location(BaseFieldSet):
+    country: Country
+    adress: Address | None = None
+    state: Any | None = None
     
-    delilvery_date: str
-    location: str
-    positions: list[dict[str, Any]]
-    shipping_costs: str
-    shipping_method: str
+
+class CartDelivery(BaseFieldSet):
+    delilvery_date: Any | None = None
+    location: Location | None = None
+    positions: list[DeliveryPosition] | None = None
+    shipping_costs: ShippingCosts | None = None #ShippingCosts 
+    shipping_method: Any | None = None
 
 
 class Cart(ApiModelBase["CartEndpoint"]):
@@ -64,7 +95,7 @@ class Cart(ApiModelBase["CartEndpoint"]):
     price: Price
     line_items: list[LineItem]
     errors: list[dict[str, Any]]
-    deliveries: list[dict[str, CartDelivery]]
+    deliveries: list[CartDelivery]
     transactions: list[dict[str, Any]]
     modified: bool
     customer_comment: str | None = None
