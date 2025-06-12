bbe7b072f17a6e639c610416b7b3a21dc46d17db
xgwxsaj <alexander.sajzew@gws.ms>
2025-06-12 22:11:55 +0000

    wip: mypy-silencer (this is fine..)

--- src/shopware_api_client/endpoints/store/core/unit.py
+++ src/shopware_api_client/endpoints/store/core/unit.py
@@ -2,5 +2,5 @@ from ....endpoints.admin.core.unit import UnitBase
 
 
 class Unit(UnitBase):
-    short_code: str | None
-    name: str | None
+    short_code: str | None = None  # type: ignore
+    name: str | None = None  # type: ignore
